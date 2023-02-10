0010703fe06eaeef70e317544d0ba341b3a909bc
Oluwayomi Oke <okeoluwayomi9@gmail.com>
2023-02-10 20:05:07 +0100

    Updating Sanity Schema

--- schemas/category.js
+++ schemas/category.js
@@ -1,19 +1,20 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from "sanity";
 
 export default defineType({
-  name: 'category',
-  title: 'Category',
-  type: 'document',
-  fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    }),
-  ],
-})
+	name: "category",
+	title: "Menu Category",
+	type: "document",
+	fields: [
+		defineField({
+			name: "name",
+			title: "Category name",
+			type: "string",
+			validation: (Rule) => Rule.required(),
+		}),
+		defineField({
+			name: "image",
+			title: "Image of Category",
+			type: "image",
+		}),
+	],
+});
--- schemas/restaurant.js
+++ schemas/restaurant.js
@@ -1,65 +1,74 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from "sanity";
 
 export default defineType({
-  name: 'post',
-  title: 'Post',
-  type: 'document',
-  fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
-    }),
-    defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
-    }),
-    defineField({
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    }),
-    defineField({
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
-    }),
-    defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    }),
-    defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
-    }),
-  ],
+	name: "restaurant",
+	title: "Restaurant",
+	type: "document",
+	fields: [
+		defineField({
+			name: "name",
+			title: "Restaurant name",
+			type: "string",
+			validation: (Rule) => Rule.required(),
+		}),
+		defineField({
+			name: "short_description",
+			title: "Short description",
+			type: "string",
+			validation: (Rule) => Rule.max(200),
+		}),
+		defineField({
+			name: "image",
+			title: "Image of the Restaurant",
+			type: "image",
+		}),
+		defineField({
+			name: "lat",
+			title: "Latitude of the Restaurant",
+			type: "number",
+		}),
+		defineField({
+			name: "long",
+			title: "Longitutde of the Restaurant",
+			type: "number",
+		}),
+		defineField({
+			name: "address",
+			title: "Restaurant address",
+			type: "string",
+			validation: (Rule) => Rule.required(),
+		}),
+		defineField({
+			name: "rating",
+			title: "Enter a Rating from 1-5 stars",
+			type: "string",
+			validation: (Rule) =>
+				Rule.required().min(1).max(5).error("Please enter a value between 1 and 5"),
+		}),
+		defineField({
+			name: "type",
+			title: "Category",
+			type: "reference",
+			validation: (Rule) => Rule.required(),
+			to: [{ type: "category" }],
+		}),
+		defineField({
+			name: "dishes",
+			title: "Dishes",
+			type: "array",
+			of: [{ type: "reference", to: [{ type: "category" }] }],
+		}),
+	],
 
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
-    },
-    prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
-    },
-  },
-})
+	// preview: {
+	//   select: {
+	//     title: 'title',
+	//     author: 'author.name',
+	//     media: 'mainImage',
+	//   },
+	//   prepare(selection) {
+	//     const {author} = selection
+	//     return {...selection, subtitle: author && `by ${author}`}
+	//   },
+	// },
+});
