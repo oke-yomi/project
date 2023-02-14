136372c4b1161992a5ff46eb4e247a7be0dc1914
Oluwayomi Oke <okeoluwayomi9@gmail.com>
2023-02-14 01:41:46 +0100

    add Restaurant screen

--- App.js
+++ App.js
@@ -2,19 +2,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
+import RestaurantScreen from "./src/screens/RestaurantScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
 		<>
-			<StatusBar barStyle="dark-content" backgroundColor='white' />
+			<StatusBar barStyle="dark-content" backgroundColor="white" />
 			<NavigationContainer>
 				<Stack.Navigator>
-					<Stack.Screen
-						name="Home"
-						component={HomeScreen}
-					/>
+					<Stack.Screen name="Home" component={HomeScreen} />
+					<Stack.Screen name="Restaurant" component={RestaurantScreen} />
 				</Stack.Navigator>
 			</NavigationContainer>
 		</>
