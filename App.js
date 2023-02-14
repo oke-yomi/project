8661228f7dabcfd6f757e7f08454df906a5b5697
Oluwayomi Oke <okeoluwayomi9@gmail.com>
2023-02-14 23:42:10 +0100

    Order page

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import RestaurantScreen from "./src/screens/RestaurantScreen";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import BasketScreen from "./src/screens/BasketScreen";
+import PreparingOrderScreen from "./src/screens/PreparingOrderScreen";
+import DeliveryScreen from "./src/screens/DeliveryScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -22,6 +24,16 @@ export default function App() {
 							name="Basket"
 							component={BasketScreen}
 							options={{ presentation: "modal", headerShown: false }}
+						/>
+						<Stack.Screen
+							name="PreparingOrder"
+							component={PreparingOrderScreen}
+							options={{ presentation: "fullScreenModal", headerShown: false }}
+						/>
+						<Stack.Screen
+							name="Delivery"
+							component={DeliveryScreen}
+							options={{ presentation: "fullScreenModal", headerShown: false }}
 						/>
 					</Stack.Navigator>
 				</Provider>
--- src/screens/BasketScreen.jsx
+++ src/screens/BasketScreen.jsx
@@ -118,7 +118,9 @@ const BasketScreen = () => {
 							</Text>
 						</View>
 
-						<TouchableOpacity className="rounded-lg bg-[#00ccbb] p-4">
+						<TouchableOpacity
+							onPress={() => navigation.navigate("PreparingOrder")}
+							className="rounded-lg bg-[#00ccbb] p-4">
 							<Text className="text-center text-white text-lg font-bold">
 								Place Order
 							</Text>
